f64a564609301e37f8e52a7451faa5c7a4a7078d
sirWalace <100574221+sirWalace@users.noreply.github.com>
2022-05-10 16:16:59 +0300

    Update mainproj.py

--- mainproj.py
+++ mainproj.py
@@ -1,67 +1,57 @@
-from kivy.app import App
-from kivy.uix.behaviors import ButtonBehavior
-from kivy.uix.image import Image
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Основное окно виджетов <статично>
-class MainWindow(Screen):
-    def sleep_on(self):
-        self.ids.sleep_image.source = 'images/sleep_turn+.png'
-    def sleep_off(self):
-        self.ids.sleep_image.source = 'images/sleep.png'
-    def tasks_on(self):
-        self.ids.tasks_image.source = 'images/task_turn+.png'
-    def tasks_off(self):
-        self.ids.tasks_image.source = 'images/task.png'
-        self.root_window = MondayWindow
-    def goal_on(self):
-        self.ids.goal_image.source = 'images/goal_turn+.png'
-    def goal_off(self):
-        self.ids.goal_image.source = 'images/goal.png'
-    def red_on(self):
-        self.ids.bip_red_image.source = 'images/bip_red.png'
-    def red_off(self):
-        self.ids.bip_red_image.source = 'images/bip_red.png'
-    def yellow_on(self):
-        self.ids.bip_yellow_image.source = 'images/bip_yellow.png'
-    def yellow_off(self):
-        self.ids.bip_yellow_image.source = 'images/bip_yellow.png'
-    def green_on(self):
-        self.ids.bip_green_image.source = 'images/bip_green.png'
-    def green_off(self):
-        self.ids.bip_green_image.source = 'images/bip_green.png'
+#only for sleep hints:
+from sleep_hints import hints, links
+from random import randint
+import webbrowser
+import matplotlib.pyplot as plt
+import numpy as np
 
 
+class MainApp(MDApp):
 
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return Builder.load_file('TheLab.kv')
 
-
-class MondayWindow(Screen):
+class MenuScreen(Screen):
     pass
 
-class TuesdayWindow(Screen):
+class NoteScreen(Screen):
     pass
+
+
+########
+i = randint(0, 8)
+
+
+def open_link():
+    global i
+    webbrowser.open(links[i])
+    return i
+#########
+
+class SleepScreen(Screen):
+    hint = hints[i]
+
+    def prove(instance):
+        open_link()
+
+class TaskScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(NoteScreen(name='note'))
+sm.add_widget(SleepScreen(name='sleep'))
+sm.add_widget(TaskScreen(name='task'))
 
 class WindowManager(ScreenManager):
     pass
 
-# Приложение, запускает kv файл
-class TheApp(App):
-    def build(self):
-        return kv
 
+MainApp().run()
 
-class RootWidget(FloatLayout):
-    pass
-
-class ImButton(ButtonBehavior, Image):
-    def on_press(self):
-        print ('pr')
-
-
-kv = Builder.load_file("TheLab.kv")
-
-
-if __name__ == "__main__":
-    TheApp().run()
